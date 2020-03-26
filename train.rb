8a1dbd8a6d0bdd1341b3811981c6f84674242a8e
Alex Dev <alex_dev_2020@mail.ru>
2020-03-26 23:58:06 +1000

    Added validation for train number

--- train.rb
+++ train.rb
@@ -5,11 +5,7 @@ class Train
   include MadeBy
   include InstanceCounter
   @@list = {}
-
-
-  def self.list
-    @@list
-  end
+  RGXP_TRAIN_NUMBER_FORMAT = /^[a-zа-я\d]{3}-?[a-zа-я\d]{2}$/i
 
   def initialize (name, number, made_by)
     @name = name
@@ -17,12 +13,29 @@ class Train
     @made_by = made_by
     @wagons = []
     @speed = 0
+    validate!
     @@list[number] = self
     register_instance
+  end
+
+  def validate!
+    raise StandardError, "Неправильный формат номера (#{self.number})" if self.number !~ RGXP_TRAIN_NUMBER_FORMAT
+  end
+
+  def valid?
+    self.validate!
+    true
+  rescue
+    false
   end
 
   def self.find(number)
     @@list[number]
   end
+
+  def self.list
+    @@list
+  end
+
 end
 
